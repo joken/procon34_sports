1ce820b581fb6103ef98cc8f5b5da09180868884
miku <miku71041@gmail.com>
2023-08-20 01:31:36 +0900

    fixed bugs

--- main.cpp
+++ main.cpp
@@ -3,45 +3,60 @@
 using namespace std;
 
 class printMap{
-    public:
-    void printMapInfo(string gameMap,int widthSize,int heightSize){
-        for(int i = 0; i < heightSize; i++){
-        for(int j = 0; j <widthSize; j++){
-            cout << gameMap.at(i).at(j);
+
+public:
+
+    void printMapInfo(string gameMap[], int widthSize, int heightSize){
+        for (int i = 0; i < heightSize; i++){
+            for (int j = 0; j < widthSize; j++){
+
+                std::cout << gameMap[i][j];
+
+            }
+
+            std::cout << " " << std::endl;
+
         }
-        cout << " " << endl;
-    }
+
     }
 };
 
-class 
+class moveBuilder{
+public:
+    void move(){
+
+    }
+};
 
 int main(){
     printMap out;
 
-    int v1 = rand() % 10 +1;
-    int v2 = rand() % 10 +1;
+    int v1 = rand() % 10 + 1;
+    int v2 = rand() % 10 + 1;
 
-    vector<vector<string> > map(v1, vector<string>(v2));
+    string map[v1][v2];
 
     for (int i = 0; i < v1; i++){
         for (int j = 0; j < v2; j++){
-            map.at(i).at(j) = "* ";
+
+            map[i][j] = "* ";
+
         }
     }
 
-    map.at(rand() % 10).at(rand() % 10) = "c1 ";
-    map.at(rand() % 10).at(rand() % 10) = "c2 ";
+    map[rand() % 10][rand() % 10] = "c1 ";
+    map[rand() % 10][rand() % 10] = "c2 ";
 
-    map.at(rand() % 10).at(rand() % 10) = "b1 ";
-    map.at(rand() % 10).at(rand() % 10) = "b2 ";
-    map.at(rand() % 10).at(rand() % 10) = "b3 ";
+    map[rand() % 10][rand() % 10] = "b1 ";
+    map[rand() % 10][rand() % 10] = "b2 ";
+    map[rand() % 10][rand() % 10] = "b3 ";
 
-    map.at(rand() % 10).at(rand() % 10) = "e1 ";
-    map.at(rand() % 10).at(rand() % 10) = "e2 ";
-    map.at(rand() % 10).at(rand() % 10) = "e3 ";
+    map[rand() % 10][rand() % 10] = "e1 ";
+    map[rand() % 10][rand() % 10] = "e2 ";
+    map[rand() % 10][rand() % 10] = "e3 ";
 
-    out.printMapInfo(map,v1,v2);
+    out.printMapInfo(map[v2], v1, v2);
 
-    cout << v1 << v2 <<endl;
+    std::cout << v1 << v2 << std::endl;
+    
 }
