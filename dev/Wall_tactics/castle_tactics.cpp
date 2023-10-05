1122d750b1817746eff229e238f619e61da08183
Gisuperu <gisubright@gmail.com>
2023-10-06 03:37:40 +0900

    城郭計画アルゴリズム完成

--- dev/Wall_tactics/castle_tactics.cpp
+++ dev/Wall_tactics/castle_tactics.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <iostream>
 #include <deque>
+#include <random>
 
 void alart(){
     std::cerr << "hoge" << std::endl;
@@ -33,6 +34,7 @@ void alart(int a1, int a2){
 struct fieldmap{
     int width; //11~25
     int height;//11~25
+    int masonNum;//陣ごとの職人数
     int castleRate;
     int territoryRate;
     int wallRate;
@@ -51,11 +53,28 @@ struct wallplan{
     std::vector<std::vector<int> > walls;
 };
 
+//　近傍のマクロ
+// 上から時計回り
+int dx4[4] = {0, 1, 0, -1};
+int dy4[4] = {1, 0, -1, 0};
+
+int dx8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+int dy8[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+std::random_device rd;
+std::mt19937 gen(rd());
+ 
+int random(int low, int high)
+{
+    std::uniform_int_distribution<> dist(low, high);
+    return dist(gen);
+}
 
 fieldmap getmap(){ //ダミーデータ
     fieldmap map = {
         11,
         11,
+        2,
         100,
         30,
         10,
@@ -102,10 +121,28 @@ fieldmap getmap(){ //ダミーデータ
     return map;
 }
 
+std::vector<std::pair<int, int> > getMasons(fieldmap const *map, int n/*職人の人数*/){
+    int width = map->width;
+    int height = map->height;
+
+    std::vector<std::pair<int, int>> masons = std::vector<std::pair<int, int>>(n);
+
+    for(int i = 0; i < width; i++){
+        for(int j =0; j < height; j++){
+            if(map->objects[i][j] > 0){
+                masons[map->objects[i][j] -1] = std::make_pair(i, j);
+            }
+        }
+    }
+
+    return masons;
+}
+
 void showMap(fieldmap const *map){
     std::cout << "=========================" << std::endl;
     std::cout << "width : " << map->width << std::endl;
     std::cout << "height: " << map->height << std::endl;
+    std::cout << "masonNum: " << map->masonNum << std::endl;
     std::cout << "castle Rate   : " << map->castleRate << std::endl;
     std::cout << "territory Rate: " << map->territoryRate << std::endl;
     std::cout << "wall Rate     : " << map->wallRate << std::endl;
@@ -172,25 +209,215 @@ void showMap(fieldmap const *map){
     std::cout << "=========================" << std::endl;
 }
 
-wallplan planning(fieldmap const *map){ // ダミーデータ
-    wallplan plan;
-    plan.width = map->width;
-    plan.height = map->height;
-    std::vector<std::vector<int> > walls = {
-        {0,0,0,0,0,0,0,0,0,0,0},
-        {0,0,1,1,2,0,0,0,0,0,0},
-        {0,0,2,0,0,2,2,2,0,0,0},
-        {0,0,0,2,2,0,0,2,0,1,0},
-        {0,2,2,0,0,0,0,2,0,1,0},
-        {0,2,0,0,0,0,0,0,2,0,0},
-        {0,2,0,0,0,0,0,2,0,0,0},
-        {2,0,0,0,0,0,0,2,0,0,0},
-        {0,2,2,0,0,0,0,2,0,0,0},
-        {0,0,0,2,0,0,0,2,0,0,0},
-        {0,0,0,0,2,2,2,2,0,0,0},
-    };
+void showPlan(wallplan const *plan){
+    std::cout << "=========================" << std::endl;
+    std::cout << "width : " << plan->width << std::endl;
+    std::cout << "height: " << plan->height << std::endl;
+    std::cout << "--- wallplan map ---" << std::endl;
+    for(std::vector<int> obj : plan->walls){
+        for(int c : obj){
+            char cell = ' ';
+            switch (c){
+            case 0: //None
+                cell = '.'; break;
+            case 1: //exist wall
+                cell = 'X'; break;
+            case 2: //planed wall
+                cell = '+'; break;
+            case 3: //planed wall
+                cell = '*'; break;
+            default: //その他
+                // cell = '.';
+                cell = '@';
+                break;
+            }
+            std::cout << cell;
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "=========================" << std::endl;
+}
 
-    plan.walls = walls;
+// wallplan planning(fieldmap const *map){ // ダミーデータ
+//     wallplan plan;
+//     plan.width = map->width;
+//     plan.height = map->height;
+//     std::vector<std::vector<int> > walls = {
+//         {0,0,0,0,0,0,0,0,0,0,0},
+//         {0,0,1,1,2,0,0,0,0,0,0},
+//         {0,0,2,0,0,2,2,2,0,0,0},
+//         {0,0,0,2,2,0,0,2,0,1,0},
+//         {0,2,2,0,0,0,0,2,0,1,0},
+//         {0,2,0,0,0,0,0,0,2,0,0},
+//         {0,2,0,0,0,0,0,2,0,0,0},
+//         {2,0,0,0,0,0,0,2,0,0,0},
+//         {0,2,2,0,0,0,0,2,0,0,0},
+//         {0,0,0,2,0,0,0,2,0,0,0},
+//         {0,0,0,0,2,2,2,2,0,0,0},
+//     };
+//     plan.walls = walls;
+//     return plan;
+// }
+
+wallplan planning(fieldmap const *map){
+    int width = map->width;
+    int height = map->height;
+
+    wallplan plan = {
+        width,
+        height,
+        std::vector<std::vector<int> >(width, std::vector<int>(height, 4))
+    };
+    //既存の壁をコピー
+    for(int i = 0; i < width; i++){
+        for(int j = 0; j < height; j++){
+            if((map->territory[i][j] & 1) == 1){
+                plan.walls[i][j] = 1;
+            }
+        }
+    }
+
+    // マップの壁を置く比重(比重の単位は[%])
+    const int BASE_WEIGHT = 80;
+    const int CASTLE_WEIGHT = (BASE_WEIGHT * (100 - abs(map->castleRate - map->wallRate)))/100;
+    std::vector<std::vector<int> > weightmap(width, std::vector<int>(height, BASE_WEIGHT));
+    for(int i = 0; i < width; i++){
+        for(int j = 0; j < height; j++){
+            int cell = map->area[i][j];
+            if(cell == 2){ //城に壁は建てづらくする
+                weightmap[i][j] = CASTLE_WEIGHT;
+            }else if(cell == 1){
+                //4方が池なら建築不可領域
+                bool canBuild = false;
+                for(int k = 0; k < 4; k++){
+                    int x = i + dx4[k];
+                    int y = j + dy4[k];
+                    if(x < 0 || x >= width || y < 0 || y >= height){
+                        continue;
+                    }
+                    if(map->area[x][y] != 1){
+                        canBuild = true;
+                        break;
+                    }
+                }
+                if(canBuild){
+                    weightmap[i][j] = BASE_WEIGHT;
+                }else{
+                    weightmap[i][j] = 0;
+                }
+            }else{
+                weightmap[i][j] = BASE_WEIGHT;
+            }
+        }
+    }
+
+    //仮壁案をweightmapに基づいて作成
+    for(int i = 0; i < width; i++){
+        for(int j = 0; j < height; j++){
+            if(plan.walls[i][j] == 1){//既に壁があるならスキップ
+                continue;
+            }else if(random(1,100) <= weightmap[i][j]){
+                plan.walls[i][j] = 3; //仮壁案
+            }
+        }
+    }
+
+    // >領域内の壁の削除処理
+    // 仮壁込みで領域を塗り潰し
+    // マップの外周に4近傍で隣接する領域を領地以外に上書きする(深さ優先探索)
+    std::deque<std::pair<int, int> > head;
+    // 外周を始点とする
+    for(int i = 0; i < width; i++){
+        head.push_back(std::make_pair(i, 0));
+        head.push_back(std::make_pair(i, height-1));
+    }
+    for(int j = 1; j < height -1; j++){
+        head.push_back(std::make_pair(0, j));
+        head.push_back(std::make_pair(width-1, j));
+    }
+    // 深さ優先探索
+    while(!head.empty()){
+        std::pair<int, int> here = head.back();
+        int x = here.first;
+        int y = here.second;
+        // alart(x, y);
+        head.pop_back();
+        if(plan.walls[x][y] == 4){
+            plan.walls[x][y] = 0; 
+            //4近傍を探索(field外は除外)
+            if((0< x-1 && x-1 < width-1) && (0< y && y < height-1)){// 上
+                head.push_back(std::make_pair(x-1, y));
+            }
+            if((0< x+1 && x+1 < width-1) && (0< y && y < height-1)){// 下
+                head.push_back(std::make_pair(x+1, y));
+            }
+            if((0< x && x < width-1) && (0< y-1 && y-1 < height-1)){// 左
+                head.push_back(std::make_pair(x, y-1));
+            }
+            if((0< x && x < width-1) && (0< y+1 && y+1 < height-1)){// 右
+                head.push_back(std::make_pair(x, y+1));
+            }
+        }else if(plan.walls[x][y] == 3){
+            plan.walls[x][y] = 2; //境界の壁を正式な壁(仮)に変更
+        }
+    }
+    showPlan(&plan);
+
+    //この段階で 既壁 : 1, 仮壁 : 3, 領域 : 4　で案ができている.
+    //線状の仮壁を削除
+    for(int i = 0; i < width; i++){
+        for(int j = 0; j < height; j++){
+            if(plan.walls[i][j] >= 1){// 既に壁が存在するならば線状かの判定を行う
+                bool isLine = false;
+                for(int k = 0; k < 4; k++){
+                    int x = i + dx8[k];
+                    int xOpp = i + dx8[k+4];
+                    int y = j + dy8[k];
+                    int yOpp = j + dy8[k+4];
+                    int target, opposit;
+
+                    if(x < 0 || x >= width || y < 0 || y >= height){
+                        target = 0;
+                    }else{
+                        target = plan.walls[x][y];
+                    }
+                    if(xOpp < 0 || xOpp >= width || yOpp < 0 || yOpp >= height){
+                        opposit = 0;
+                    }else{
+                        opposit = plan.walls[xOpp][yOpp];
+                    }
+
+                    if(target == 0 && opposit == 0){
+                        isLine = true;
+                        break;
+                    }
+                }
+                if(isLine){
+                    plan.walls[i][j] = 0;
+                    for(int k = 0; k < 4; k++){
+                        int x = i + dx4[k];
+                        int y = j + dy4[k];
+                        if(x < 0 || x >= width || y < 0 || y >= height){
+                            continue;
+                        }
+                        if(plan.walls[x][y] > 2){
+                            plan.walls[x][y] = 2;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // 外周以外の仮壁，領域の削除
+    for(int i = 0; i < width; i++){
+        for(int j = 0; j < height; j++){
+            if(plan.walls[i][j] > 2){ //仮設定の壁とか領域を削除
+                plan.walls[i][j] = 0;
+            }
+        }
+    }
+
     return plan;
 }
 
@@ -215,25 +442,25 @@ int calcTerritoryPoint(fieldmap const *map, wallplan const *plan){
     }
 
 
-    for(std::vector<int> trr : territory){
-        for(int c : trr){
-            char cell = ' ';
-            switch (c){
-            case 0: 
-                cell = '.'; break;
-            case -1: 
-                cell = 'X'; break;
-            case 1: 
-                cell = '0'; break;
-            default: //その他
-                // cell = '.';
-                cell = '+';
-                break;
-            }
-            std::cout << cell;
-        }
-        std::cout << std::endl;
-    }
+    // for(std::vector<int> trr : territory){
+    //     for(int c : trr){
+    //         char cell = ' ';
+    //         switch (c){
+    //         case 0: 
+    //             cell = '.'; break;
+    //         case -1: 
+    //             cell = 'X'; break;
+    //         case 1: 
+    //             cell = '0'; break;
+    //         default: //その他
+    //             // cell = '.';
+    //             cell = '+';
+    //             break;
+    //         }
+    //         std::cout << cell;
+    //     }
+    //     std::cout << std::endl;
+    // }
 
     // マップの外周に4近傍で隣接する領域を領地以外に上書きする(深さ優先探索)
     std::deque<std::pair<int, int> > head;
@@ -271,25 +498,25 @@ int calcTerritoryPoint(fieldmap const *map, wallplan const *plan){
         }
     }
 
-    for(std::vector<int> trr : territory){
-        for(int c : trr){
-            char cell = ' ';
-            switch (c){
-            case 0: 
-                cell = '.'; break;
-            case -1: 
-                cell = 'X'; break;
-            case 1: 
-                cell = '0'; break;
-            default: //その他
-                // cell = '.';
-                cell = '+';
-                break;
-            }
-            std::cout << cell;
-        }
-        std::cout << std::endl;
-    }
+    // for(std::vector<int> trr : territory){
+    //     for(int c : trr){
+    //         char cell = ' ';
+    //         switch (c){
+    //         case 0: 
+    //             cell = '.'; break;
+    //         case -1: 
+    //             cell = 'X'; break;
+    //         case 1: 
+    //             cell = '0'; break;
+    //         default: //その他
+    //             // cell = '.';
+    //             cell = '+';
+    //             break;
+    //         }
+    //         std::cout << cell;
+    //     }
+    //     std::cout << std::endl;
+    // }
 
     //ポイント計算
     int area = 0;
@@ -326,6 +553,7 @@ int main(){
     
     // 2.城郭の計画を立案
     wallplan plan = planning(&map);
+    showPlan(&plan);
 
     // 3.城郭の得点計算
     int point = calcTerritoryPoint(&map, &plan);
